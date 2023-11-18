8dfafd824a8a2756a55207f14cc7f79f53210545
Ashish Verma <ashishverma510207@gmail.com>
2023-11-18 18:39:02 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CssBaseline from "@mui/material/CssBaseline"
+import TodoList from "./TodoList";
+import Navbar from "./Navbar";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-        <CssBaseline/>
-        <h1>Todos</h1>
+        <CssBaseline />
+        <Navbar />
+        <TodoList />
     </>
   )
 }
